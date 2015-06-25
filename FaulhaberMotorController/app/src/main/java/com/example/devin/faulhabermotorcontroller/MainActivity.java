840b1841df9a587667e4de0d053e10c9649d8a0a
devinhaber <devinhaber@yahoo.com>
2015-06-25 17:00:28 -0400

    Main page layout updated; Initial bluetooth setup added

--- FaulhaberMotorController/app/src/main/java/com/example/devin/faulhabermotorcontroller/MainActivity.java
+++ FaulhaberMotorController/app/src/main/java/com/example/devin/faulhabermotorcontroller/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.devin.faulhabermotorcontroller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    public final static int REQUEST_ENABLE_BT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +35,39 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if(id == R.id.action_connect) {
+            BluetoothAdapter btadapter = BluetoothAdapter.getDefaultAdapter();
+            if(btadapter == null){
+                TextView textview = new TextView(this);
+                textview.setTextSize(40);
+                textview.setText("BlueTooth is NOT enabled");
+                RelativeLayout relative =(RelativeLayout) findViewById(R.id.mainlayout);
+                relative.addView(textview);
+
+            } else {
+                if(!btadapter.isEnabled()){
+                    Intent enablebtintent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enablebtintent,REQUEST_ENABLE_BT);
+                } else{
+                    Intent connectintent = new Intent(this, ConnectActivity.class);
+                    startActivity(connectintent);
+                }
+            }
+
+            return true;
         }
 
+
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if (resultCode == -1){
+           Intent connectintent = new Intent(this, ConnectActivity.class);
+           startActivity(connectintent);
+       } else{
+           return;
+       }
     }
 }
